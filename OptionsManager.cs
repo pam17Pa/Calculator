656e3dea7e47b766c0ffaf95aeefbf0e7ffadf64
pam17Pa <pa23524184@edu1.pgzaimov.com>
2022-01-16 20:22:43 +0200

    Adding

--- OptionsManager.cs
+++ OptionsManager.cs
@@ -5,14 +5,29 @@ namespace GitExercise
     public static class OptionsManager
     {
         public static string[] OptionsList = {
-            "a - Add"
+            "a - Add",
+            "s - Substract",
+            "m - Multiply",
+            "d - Divide"
         };
 
         public static void Add(double a, double b)
         {
             Console.WriteLine($"{a} + {b} = {a + b}");
         }
+        public static void Substract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} * {b} = {a * b}");
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
 
-       
+
     }
 }
